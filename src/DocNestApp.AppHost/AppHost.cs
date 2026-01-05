7dfbd121b54b5153e6b81408196124f74c6db8d7
Broodco <broodcoorens.maxime@gmail.com>
2026-01-05 13:28:14 +0100

    Create API project Add postgresql db and wired it to the api with aspire

--- src/DocNestApp.AppHost/AppHost.cs
+++ src/DocNestApp.AppHost/AppHost.cs
@@ -1,3 +1,13 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var postgres = builder.AddPostgres("postgres")
+    .WithPgAdmin()
+    .WithLifetime(ContainerLifetime.Persistent);
+
+var docnestdb = postgres.AddDatabase("docnest-db");
+
+builder.AddProject<Projects.DocNestApp_Api>("docnest-api")
+    .WithReference(docnestdb)
+    .WaitFor(postgres);
+    
 builder.Build().Run();
